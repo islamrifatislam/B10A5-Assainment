6d93399ad4e24cfa8727811b4e9392575805b096
islamrifatislam <islamrifatislam500@gmail.com>
2024-09-24 17:41:39 +0600

    added a function ,blog,Model section

--- js/blog.js
+++ js/blog.js
@@ -25,3 +25,38 @@ addDonation("btn-donate-1", "donation-noakhali", "total-donation-noakhali", "mai
 addDonation("btn-donate-2", "donation-feni", "total-donation-feni", "main-balance")
 // section3
 addDonation("btn-donate-3", "donation-quota", "total-donation-qouta", "main-balance")
+
+// toogle sestion
+const historyTab = document.getElementById('history-tab')
+const donationTab = document.getElementById('donation-tab')
+
+historyTab.addEventListener('click', function () {
+historyTab.classList.add('bg-primary_btn');
+donationTab.classList.remove('bg-primary_btn');
+document.getElementById("history-container").classList.remove('hidden')
+document.getElementById("donation-main").classList.add('hidden')
+})
+
+
+
+// toggle donate
+donationTab.addEventListener('click', function () {
+donationTab.classList.add('bg-primary_btn')
+historyTab.classList.remove('bg-primary_btn')
+document.getElementById("history-container").classList.add('hidden')
+document.getElementById("donation-main").classList.remove('hidden')
+})
+
+
+function modalShow(button, donationFieldINput) {
+document.getElementById(button).addEventListener('click', function () {
+const donationInput = getValueById(donationFieldINput)
+const mainBalance = getInnerTextById('main-balance')
+if (isNaN(donationInput) === false && donationInput > 0 && donationInput < mainBalance) {
+return my_modal.showModal();
+}
+})
+}
+modalShow("btn-donate-1", "donation-noakhali")
+modalShow("btn-donate-2", "donation-feni")
+modalShow("btn-donate-3", "donation-quota")
